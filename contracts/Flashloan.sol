95494fad403b5aa7158a65d61a74ff284c7da878
“Fidelion” <david.d.95@hotmail.com>
2022-03-21 23:49:28 +0800

    Flashloan Contract modified

--- contracts/Flashloan.sol
+++ contracts/Flashloan.sol
@@ -7,6 +7,10 @@ import "@studydef/money-legos/dydx/contracts/ICallee.sol";
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+import "./IUniswapV2Router02.sol";
+import "./IWeth";
+import { KyberNetworkProxy as IKyberNetworkProxy } from '@studydefi/money-legos/kyber/contracts/KyberNetworkProxy.sol';
+
 contract Flashloan is ICallee, DydxFlashloanBase {
     enum Direction { KyberToUniswap, UniswapToKyber }
 
@@ -15,13 +19,40 @@ contract Flashloan is ICallee, DydxFlashloanBase {
         uint256 repayAmount;
     }
 
+    IKyberNetworkProxy kyber;
+    IUniswapV2Router02 uniswap;
+    IWeth weth;
+    IERC20 dai;
+    address constant KYBER_ETH_ADDRESS = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+
+    constructor(
+        kyberAddress,
+        uniswapAddress,
+        wethAddress,
+        daiAddress
+    ) {
+        kyber = IKyberNetworkProxy(kyberAddress);
+        uniswap = IUniswapV2Router02(uniswapAddress);
+        weth = IWeth(wethAddress);
+        dai = IERC20(daiAddress);
+    }
+
     function callFunction(
         address sender,
         Account.Info memory account,
         bytes memory data
     ) public {
         ArbInfo memory arbInfo = abi.decode(data, (ArbInfo));
-      
+        uint256 balanceDai = dai.balanceOf(address(this));
+
+        // Note that you can ignore the line below
+        // if your dydx account (this contract in this case)
+        // has deposited at least ~2 Wei of assets into the account
+        // to balance out the collaterization ratio
+        require(
+            balanceDai >= arbInfo.repayAmount,
+            "Not enough funds to repay dydx loan!"
+        );
     }
 
     function initiateFlashloan(
