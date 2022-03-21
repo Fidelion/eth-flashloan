7294099ca2a8310ec94f46430724b5a2a1147711
“Fidelion” <david.d.95@hotmail.com>
2022-03-21 21:33:32 +0800

    Infura URL Setup

--- run-arbitrage.js
+++ run-arbitrage.js
@@ -1,2 +1,15 @@
+require("dotenv").config();
+
 const Web3 = require("web3");
 const web3 = new Web3(process.env.INFURA_URL);
+
+const web3 = new Web3(process.env.INFURA_URL);
+
+web3.eth
+	.subscribe("newBlockHeaders")
+	.on("data", async (block) => {
+		console.log(`New Block. Block # ${block.number}`);
+	})
+	.on("error", (error) => {
+		console.log(error);
+	});
