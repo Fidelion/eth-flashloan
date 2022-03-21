12f6073a0530c3f3b80728db49f9513909394fbd
“Fidelion” <david.d.95@hotmail.com>
2022-03-21 21:38:04 +0800

    Kyber Config added

--- run-arbitrage.js
+++ run-arbitrage.js
@@ -1,9 +1,20 @@
 require("dotenv").config();
-
 const Web3 = require("web3");
+const abis = require("./abis");
+const { mainnet: addresses } = require("./addresses");
 const web3 = new Web3(process.env.INFURA_URL);
 
-const web3 = new Web3(process.env.INFURA_URL);
+const kyber = new web3.eth.Contract(
+	abis.kyber.kyberNetworkProxy,
+	addresses.kyber.kyberNetworkProxy
+);
+
+const AMOUNT_ETH = 100;
+const RECENT_ETH_PRICE = 2600;
+const AMOUNT_ETH_WEI = web3.utils.toWei(AMOUNT_ETH.toString());
+const AMOUNT_DAI_WEI = web3.utils.toWei(
+	(AMOUNT_ETH * RECENT_ETH_PRICE).toString()
+);
 
 web3.eth
 	.subscribe("newBlockHeaders")
